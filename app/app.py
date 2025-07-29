842f866ae3c604d436a04db2b39e0b8c57b76d0f
Batuhan kertmen <batuhankertmen@sabanciuniv.edu>
2025-07-29 19:30:15 +0300

    samll fix

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import os
+import base64
 
 app = dash.Dash(__name__)
 country_list = sorted([f.replace('.png', '') for f in os.listdir("results/changepoints") if f.endswith(".png")])
@@ -16,8 +17,10 @@ app.layout = html.Div([
     Input('country-dropdown', 'value')
 )
 def update_image(country):
-    with open(f'results/changepoints/{country}.png', 'rb') as f:
-        return "data:image/png;base64," + f.read().encode("base64").decode()
+    image_path = os.path.join("results", "changepoints", f"{country}.png")
+    with open(image_path, 'rb') as f:
+        encoded = base64.b64encode(f.read()).decode('utf-8')
+    return f"data:image/png;base64,{encoded}"
 
 if __name__ == "__main__":
     app.run_server(debug=True, host='0.0.0.0')
